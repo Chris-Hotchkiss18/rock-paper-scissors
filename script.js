9c0a1b62423ec82f0e66ca2092dd7fa03120a7a1
Chris-Hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-12 15:40:04 +0100

    Change game function to remove "undefined" answer

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 //Rock Paper Scissors Game
 
 //Computer random choice
-function computerChoice(){
-    let compChoice = ["rock", "paper", "scissors"]
-    return compChoice[Math.floor(Math.random() * compChoice.length)];
+function computerChoice() {
+  let compChoice = ["rock", "paper", "scissors"];
+  return compChoice[Math.floor(Math.random() * compChoice.length)];
 }
 
 //Player input choice
-function playerChoice(){
-    let choice = prompt("Pick Rock / Paper / Scissors");
-    return choice.toLowerCase()
+function playerChoice() {
+  let choice = prompt("Pick Rock / Paper / Scissors");
+  return choice.toLowerCase();
 }
 
 //Play round & keep score
 let playerScore = 0;
 let computerScore = 0;
 
-function playRound(answer1, answer2){
-    if(answer1 === answer2){
-        return "Its a tie! You both picked " + answer1;
-    }
-    if ((answer1 == 'rock' && answer2 == 'scissors') ||
-    (answer1 == 'scissors' && answer2 == 'paper') ||
-    (answer1 == 'paper' && answer2 == 'rock')) { playerScore++;
-        return "You win! " + answer1 + " beats " + answer2;}
-                else { computerScore ++;
-                return "You lose! " + answer2 + " beats " + answer1}
+function playRound(answer1, answer2) {
+  if (answer1 === answer2) {
+    return "Its a tie! You both picked " + answer1;
+  }
+  if (
+    (answer1 == "rock" && answer2 == "scissors") ||
+    (answer1 == "scissors" && answer2 == "paper") ||
+    (answer1 == "paper" && answer2 == "rock")
+  ) {
+    playerScore++;
+    return "You win! " + answer1 + " beats " + answer2;
+  } else {
+    computerScore++;
+    return "You lose! " + answer2 + " beats " + answer1;
+  }
 }
 
 //Repeat round until winner declared
-function playGame(){
-    while (playerScore<5 && computerScore<5){
-        playRound(playerChoice(), computerChoice());
-        console.log(playRound());
-        console.log(playerScore, computerScore);
-    }
+function playGame() {
+  while (playerScore < 5 && computerScore < 5) {
+    console.log(playRound(playerChoice(), computerChoice()));
+    console.log(playerScore, computerScore);
+  }
+  if (playerScore == 5) {
+    console.log("Player Wins! - Refresh to play again.");
+  }
+  if (computerScore == 5) {
+    console.log("Computer Wins! - Refresh to play again.");
+  }
 }
 
 playGame();
