13c27c8400fd21b7f4ea3d2bb664cd25bd3b2c14
chris hotchkiss <chris.hotchkiss18@outlook.com>
2022-05-12 14:10:25 +0100

    create html file and write most code for game

---
+++ script.js
@@ -0,0 +1,40 @@
+//Rock Paper Scissors Game
+
+//Computer random choice
+function computerChoice(){
+    let compChoice = ["rock", "paper", "scissors"]
+    return compChoice[Math.floor(Math.random() * compChoice.length)];
+}
+
+//Player input choice
+function playerChoice(){
+    let choice = prompt("Pick Rock / Paper / Scissors");
+    return choice.toLowerCase()
+}
+
+//Play round & keep score
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(answer1, answer2){
+    if(answer1 === answer2){
+        return "Its a tie! You both picked " + answer1;
+    }
+    if ((answer1 == 'rock' && answer2 == 'scissors') ||
+    (answer1 == 'scissors' && answer2 == 'paper') ||
+    (answer1 == 'paper' && answer2 == 'rock')) { playerScore++;
+        return "You win! " + answer1 + " beats " + answer2;}
+                else { computerScore ++;
+                return "You lose! " + answer2 + " beats " + answer1}
+}
+
+//Repeat round until winner declared
+function playGame(){
+    while (playerScore<5 && computerScore<5){
+        playRound(playerChoice(), computerChoice());
+        console.log(playRound());
+        console.log(playerScore, computerScore);
+    }
+}
+
+playGame();
